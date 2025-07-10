c740b6c6509c8df860595f4c3967882f6792bcf9
mazenshawky <mazenshawqy@gmail.com>
2025-07-10 16:56:27 +0300

    Add flutter_stripe package and configure its android dependencies

--- android/app/src/main/kotlin/com/example/stripe_and_paypal_payment_gateway_sample/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/stripe_and_paypal_payment_gateway_sample/MainActivity.kt
@@ -1,5 +1,5 @@
 package com.example.stripe_and_paypal_payment_gateway_sample
 
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterFragmentActivity()
